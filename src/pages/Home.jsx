e46d2e33717c59637e81ccc91465f363c6c48d54
GuyKopel1568 <guykopel1568@gmail.com>
2025-05-14 20:05:18 +0300

    Add Navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,40 +19,12 @@ function Home() {
   }, []);
 
   return (
-    <div
-      className="flex flex-col lg:flex-row w-screen min-h-screen bg-cover bg-center transition-all duration-500 items-center justify-center"
-      style={{
-        backgroundImage: isDarkMode
-          ? "url('/backgroundDark.png')"
-          : "url('/backgroundLight.png')",
-      }}
-    >
-      {/* Left column - Boarding pass */}
+    <div className=" bg-gray-900 flex flex-col lg:flex-row w-screen min-h-screen bg-cover bg-center transition-all items-center ">
       <div
         data-aos="fade-right"
-        className="z-10 flex items-center justify-center w-full lg:w-1/2 p-6"
+        className="z-10 flex items-center justify-center w-full lg:w-2/3 p-24"
       >
         <HomeHeader />
-      </div>
-
-      {/* Right column - Globe */}
-      <div
-        data-aos="fade-down"
-        className="relative z-0 flex items-center justify-center w-full lg:w-1/2 px-4 pb-10"
-      >
-        <div className="w-[300px] h-[300px] sm:w-[400px] sm:h-[400px] md:w-[500px] md:h-[500px] lg:w-[650px] lg:h-[650px]">
-          <Canvas camera={{ position: [0, 0, 8], fov: 45 }}>
-            <ambientLight intensity={isDarkMode ? 15 : 2} />
-            <directionalLight position={[5, 5, 5]} intensity={2} />
-            <Globe
-              scale={3}
-              texturePath={`/earth/${
-                isDarkMode ? 'earth_night' : 'earth_day'
-              }.jpg`}
-            />
-            <OrbitControls enableZoom={false} />
-          </Canvas>
-        </div>
       </div>
     </div>
   );
